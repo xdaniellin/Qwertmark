6f0ecda0df0700ece725b589c8a0994cdfefe0ce
Robin Lee <robincrlee@gmail.com>
2014-04-13 12:05:38 -0700

    page.js-fill in every letter's request

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -37,14 +37,33 @@ $(window).keyup(function(e) {
 
 
 var urls = new Array();
-//urls[65] = "http://www.google.com";
-//urls[83] = "http://www.facebook.com";
-//urls[68] = "http://www.quixey.com";
-//urls[70] = "http://www.lahacks.com";
-////urls[74] = "http://www.nextdoor.com";
-//urls[75] = "http://www.gmail.com";
-//urls[76] = "http://www.linkedin.com";
+urls[65] = "http://" + $('#a .bmark').text();
+urls[66] = "http://" + $('#b .bmark').text();
+urls[67] = "http://" + $('#c .bmark').text();
+urls[68] = "http://" + $('#d .bmark').text();
+urls[69] = "http://" + $('#e .bmark').text();
+urls[70] = "http://" + $('#f .bmark').text();
+urls[71] = "http://" + $('#g .bmark').text();
+urls[72] = "http://" + $('#h .bmark').text();
+urls[73] = "http://" + $('#i .bmark').text();
+urls[74] = "http://" + $('#j .bmark').text();
+urls[75] = "http://" + $('#k .bmark').text();
+urls[76] = "http://" + $('#l .bmark').text();
+urls[77] = "http://" + $('#m .bmark').text();
+urls[78] = "http://" + $('#n .bmark').text();
+urls[79] = "http://" + $('#o .bmark').text();
+urls[80] = "http://" + $('#p .bmark').text();
 urls[81] = "http://" + $('#q .bmark').text();
+urls[82] = "http://" + $('#r .bmark').text();
+urls[83] = "http://" + $('#s .bmark').text();
+urls[84] = "http://" + $('#t .bmark').text();
+urls[85] = "http://" + $('#u .bmark').text();
+urls[86] = "http://" + $('#v .bmark').text();
+urls[87] = "http://" + $('#w .bmark').text();
+urls[88] = "http://" + $('#x .bmark').text();
+urls[89] = "http://" + $('#y .bmark').text();
+urls[90] = "http://" + $('#z .bmark').text();
+
 console.log(urls[81]);
 
 	url = urls[e.keyCode];
